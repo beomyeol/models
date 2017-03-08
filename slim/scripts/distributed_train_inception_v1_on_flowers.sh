bc5a23e57bbbff7c1801c99ac11b6358a3eaf589
Beomyeol Jeon <beomyeolj@gmail.com>
2017-03-07 22:33:26 -0600

    Add a distributed training script for inception_v1 on flowers

--- slim/scripts/distributed_train_inception_v1_on_flowers.sh
+++ slim/scripts/distributed_train_inception_v1_on_flowers.sh
@@ -9,17 +9,13 @@
 # cd slim
 # ./slim/scripts/finetune_inception_v1_on_flowers.sh
 
-# Where the pre-trained InceptionV1 checkpoint is saved to.
-PRETRAINED_CHECKPOINT_DIR=/tmp/checkpoints
-
 # Where the training (fine-tuned) checkpoint and logs will be saved to.
 TRAIN_DIR=/tmp/flowers-models/inception_v1
 
 # Where the dataset is saved to.
 DATASET_DIR=/tmp/flowers
 
-ONLY_NEW_MAX_STEPS=2000
-ENTIRE_MAX_STEPS=1000
+MAX_STEPS=1000
 
 PS_HOSTS=172.17.0.2:2222
 WORKER_HOSTS=172.17.0.1:2222
@@ -51,18 +47,6 @@ if ! [[ ${TASK_ID} =~ $re ]]; then
   exit
 fi
 
-# Download the pre-trained checkpoint.
-
-if [ ! -d "$PRETRAINED_CHECKPOINT_DIR" ]; then
-  mkdir ${PRETRAINED_CHECKPOINT_DIR}
-fi
-if [ ! -f ${PRETRAINED_CHECKPOINT_DIR}/inception_v1.ckpt ]; then
-  wget http://download.tensorflow.org/models/inception_v1_2016_08_28.tar.gz
-  tar -xvf inception_v1_2016_08_28.tar.gz
-  mv inception_v1.ckpt ${PRETRAINED_CHECKPOINT_DIR}/inception_v1.ckpt
-  rm inception_v1_2016_08_28.tar.gz
-fi
-
 # Download the dataset
 python download_and_convert_data.py \
   --dataset_name=flowers \
@@ -84,39 +68,17 @@ OPTS="--type=distributed \
   --log_every_n_steps=100 \
   --optimizer=rmsprop \
   --weight_decay=0.00004 \
+  --train_dir=${TRAIN_DIR} \
+  --max_number_of_steps=${MAX_STEPS} \
+  --learning_rate=0.01 \
   --ps_on_cpu=${PS_ON_CPU}"
 
-# Fine-tune only the new layers for 2000 steps.
-ONLY_NEW_OPTS="--learning_rate=0.01 \
-  --checkpoint_path=${PRETRAINED_CHECKPOINT_DIR}/inception_v1.ckpt \
-  --checkpoint_exclude_scopes=InceptionV1/Logits \
-  --trainable_scopes=InceptionV1/Logits \
-  --train_dir=${TRAIN_DIR} \
-  --max_number_of_steps=${ONLY_NEW_MAX_STEPS} "
-
-python train_image_classifier.py ${OPTS} ${ONLY_NEW_OPTS}
+python train_image_classifier.py ${OPT}
 
 # Run evaluation.
 python eval_image_classifier.py \
   --checkpoint_path=${TRAIN_DIR} \
   --eval_dir=${TRAIN_DIR} \
-  --dataset_name=flowers \
-  --dataset_split_name=validation \
-  --dataset_dir=${DATASET_DIR} \
-  --model_name=inception_v1
-
-# Fine-tune all the new layers for 1000 steps.
-ENTIRE_OPTS="--train_dir=${TRAIN_DIR}/all \
-  --checkpoint_path=${TRAIN_DIR} \
-  --max_number_of_steps=${ENTIRE_MAX_STEPS} \
-  --learning_rate=0.001"
-
-python train_image_classifier.py ${OPTS} ${ENTIRE_OPTS}
-
-# Run evaluation.
-python eval_image_classifier.py \
-  --checkpoint_path=${TRAIN_DIR}/all \
-  --eval_dir=${TRAIN_DIR}/all \
   --dataset_name=flowers \
   --dataset_split_name=validation \
   --dataset_dir=${DATASET_DIR} \
